55d75ca6cad0fa9d463daeef8d71f8ef359830c5
viveksoni <vivek.soni@gmail.com>
2026-09-23 22:26:04 +0530

    AAdded new backend files

---
+++ part 3/2script.js
@@ -0,0 +1,34 @@
+// NPM understanding
+
+// npm - node package manager
+// node js core mein jo installed aata hai wo kehlaata hain module 
+// npm se downlode karte hai wo hota hai package
+// ex - text to speech
+// google par hai
+
+
+// installing and uninstalling anything basics & advanced
+
+// insrall -> npm i packageKaNaam     //(npm i express)
+// uninstall -> npm uninstall packageKaNaam    //(npm uninstall express)
+// installing particular versions
+
+
+// understanding node_modules
+
+// dependencies - packages and packages ki dependencies
+// devdependencies - eaise packages jo sirf development mein kaam aayege par jab aap ban jaayega aur upload ho jayega tab hum in package ko use nahi kar rahe honge
+
+
+
+// kai baar ap koi script chalate ho and us script mein aap likhte ho ye command 
+// npm start
+// npm run dev
+// npm run concurrent
+// // eaisa kyu kabhi kabhi ham script me run likhte hai kabhi kabhi nhi likhte hai  
+// => start test
+
+// script - understanding default scripts PATH and custom script 
+
+
+console.log("hello world");
